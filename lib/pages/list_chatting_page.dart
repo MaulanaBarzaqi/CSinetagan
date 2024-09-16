0d069f4e566c55dc017d1269117858ca20cbff7b
maulana barzaqi <molaarzaqi22@gmail.com>
2024-09-16 12:53:32 +0700

    chat model

---
+++ lib/pages/list_chatting_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class ListChattingPage extends StatefulWidget {
+  const ListChattingPage({super.key});
+
+  @override
+  State<ListChattingPage> createState() => _ListChattingPageState();
+}
+
+class _ListChattingPageState extends State<ListChattingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/sources/chat_source.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:csinetagan/models/chat_model.dart';
+
+class ChatSource {
+  //kalo sudah pernah chat CS
+  static Future<void> openChatRoom(String uid, String userName) async {
+    await FirebaseFirestore.instance.collection('CS').doc(uid).update({
+      'newFromUser': false,
+    });
+  }
+
+//fungfi untuk tombol kirim pesan
+  static Future<void> send(Chat chat, String uid) async {
+    //notifikasi
+    await FirebaseFirestore.instance.collection('CS').doc(uid).update({
+      'lastMessage': chat.message,
+      'newFromUser': false,
+      'newFromCS': true,
+    });
+    //funsi tombol input chat dan mengirim(user ke cs)
+    await FirebaseFirestore.instance
+        .collection('CS')
+        .doc(uid)
+        .collection('chats')
+        .add({
+      'roomId': chat.roomId,
+      'message': chat.message,
+      'receiverId': chat.receiverId,
+      'senderId': chat.senderId,
+      'bikeDetail': chat.bikeDetail,
+      'timestamp': FieldValue.serverTimestamp(),
+    });
+  }
+}
